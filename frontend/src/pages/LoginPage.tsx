cb2229fed05f41665bbc21cbb031e8d948e4ef0f
afnan <afnanballoor44@gmail.com>
2024-09-13 16:08:54 +0530

    1. sets route for login page 2. started to design login page

---
+++ frontend/src/pages/LoginPage.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const LoginPage: React.FC = () => {
+  return (
+    <div className='flex h-screen'>
+        <div className="w-1/2 bg-blue-500 flex items-center justify-center">
+            <h1 className='text-white text-3xl'></h1>
+        </div>
+
+        <div className="w-1/2 bg-red-500 flex items-center justify-center">
+            <h1 className='text-white text-3xl'></h1>
+        </div>
+    </div>
+  )
+}
+
+export default LoginPage
